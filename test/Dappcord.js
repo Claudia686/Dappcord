c1d94e2a596af098cdc8674c594a473a77e45d7a
Claudia686 <claudia_mihaela@rocketmail.com>
2023-10-31 21:40:39 +0000

    Added success test case for update users profiles

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -55,7 +55,7 @@ describe("Dappcord", function() {
   })
 
   describe("Sets user Profile", async () => {
-    it("Set and retrieve user profiles", async function() {
+    it("Set and retrieve user profiles", async () => {
       const name = "Alice";
       const bio = "Crypto enthusiast";
       const avatarUrl = "https://example.com/avatar.png";
@@ -67,60 +67,77 @@ describe("Dappcord", function() {
       expect(userProfile.bio).to.equal("Crypto enthusiast");
       expect(userProfile.avatarUrl).to.equal("https://example.com/avatar.png");
     })
-    }
 
-    describe("Joining Channels", async () => {
-      const ID = 1
-      const AMOUNT = ethers.utils.parseUnits("1", "ether")
+    it("Allow users to update their profiles", async () => {
+      const name = "Alice";
+      const updatedName = "Alice Updated";
+      const bio = "Crypto enthusiast";
+      const updatedBio = "Newbio";
+      const avatarUrl = "https://example.com/avatar.png";
+      const updatedAvatarUrl = "https://example.com/avatar-updated.png";
 
-      beforeEach(async () => {
-        const transaction = await dappcord.connect(user).mint(ID, {
-          value: AMOUNT
-        })
-        await transaction.wait()
-      })
+      await dappcord.connect(user1).setUserProfile(name, bio, avatarUrl);
+      await dappcord.connect(user1).setUserProfile(updatedName, updatedBio, updatedAvatarUrl);
 
-      it("Joins the user", async () => {
-        const result = await dappcord.hasJoined(ID, user.address)
-        expect(result).to.equal(true)
-      })
-
-      it("Increases total supply", async () => {
-        const result = await dappcord.totalSupply()
-        expect(result).to.equal(ID)
-      })
-
-      it("Updates contract balance", async () => {
-        const result = await ethers.provider.getBalance(dappcord.address)
-        expect(result).to.equal(AMOUNT)
-      })
-    })
-
-    describe("Withdrawing", () => {
-      const ID = 1
-      const AMOUNT = ethers.utils.parseUnits("10", 'ether')
-      let balanceBefore
-
-      beforeEach(async () => {
-        balanceBefore = await ethers.provider.getBalance(deployer.address)
-
-        let transaction = await dappcord.connect(user).mint(ID, {
-          value: AMOUNT
-        })
-        await transaction.wait()
-
-        transaction = await dappcord.connect(deployer).withdraw()
-        await transaction.wait()
-      })
-
-      it('Updates the owner balance', async () => {
-        const balanceAfter = await ethers.provider.getBalance(deployer.address)
-        expect(balanceAfter).to.be.greaterThan(balanceBefore)
-      })
-
-      it('Updates the contract balance', async () => {
-        const result = await ethers.provider.getBalance(dappcord.address)
-        expect(result).to.equal(0)
-      })
+      const userProfile = await dappcord.getUserProfile(user1.address)
+      expect(userProfile.name).to.equal(updatedName);
+      expect(userProfile.bio).to.equal(updatedBio);
+      expect(userProfile.avatarUrl).to.equal(updatedAvatarUrl);
     })
   })
+
+  describe("Joining Channels", async () => {
+    const ID = 1
+    const AMOUNT = ethers.utils.parseUnits("1", "ether")
+
+    beforeEach(async () => {
+      const transaction = await dappcord.connect(user).mint(ID, {
+        value: AMOUNT
+      })
+      await transaction.wait()
+    })
+
+    it("Joins the user", async () => {
+      const result = await dappcord.hasJoined(ID, user.address)
+      expect(result).to.equal(true)
+    })
+
+    it("Increases total supply", async () => {
+      const result = await dappcord.totalSupply()
+      expect(result).to.equal(ID)
+    })
+
+    it("Updates contract balance", async () => {
+      const result = await ethers.provider.getBalance(dappcord.address)
+      expect(result).to.equal(AMOUNT)
+    })
+  })
+
+  describe("Withdrawing", () => {
+    const ID = 1
+    const AMOUNT = ethers.utils.parseUnits("10", 'ether')
+    let balanceBefore
+
+    beforeEach(async () => {
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+      let transaction = await dappcord.connect(user).mint(ID, {
+        value: AMOUNT
+      })
+      await transaction.wait()
+
+      transaction = await dappcord.connect(deployer).withdraw()
+      await transaction.wait()
+    })
+
+    it('Updates the owner balance', async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address)
+      expect(balanceAfter).to.be.greaterThan(balanceBefore)
+    })
+
+    it('Updates the contract balance', async () => {
+      const result = await ethers.provider.getBalance(dappcord.address)
+      expect(result).to.equal(0)
+    })
+  })
+})
